f9ee112f18d7e4d9d9e81bd9212542555642dc40
reznikmn <rion@spark-mail.ru>
2017-12-04 17:58:34 +0300

    add word

--- vowels6.py
+++ vowels6.py
@@ -1,10 +1,21 @@
+<<<<<<< Updated upstream
 vowels=['a','e','i','o','u',]
 word=input("Provide a word to search fo vowels:")
+=======
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = input("Provide a word to search fo vowels:")
+>>>>>>> Stashed changes
 found = {}
 for letter in word:
     if letter in vowels:
         found.setdefault(letter, 0)
+<<<<<<< Updated upstream
         found[letter]+=1
 for k,v in sorted(found.items()):
     print(k, 'was found', v,'time(s).')
 print('fuck them all!')
+=======
+        found[letter] += 1
+for k, v in sorted(found.items()):
+    print(k, 'was found', v, 'time(s).')
+>>>>>>> Stashed changes
